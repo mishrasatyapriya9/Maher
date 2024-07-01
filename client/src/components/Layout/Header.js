d45dfa823b8ebba3d6fea64eeb5b85c9328b1754
mishrasatyapriya9 <mishrasatyapriya9@gmial.com>
2024-07-01 18:47:23 +0530

    addin

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../assets/icon Mehar HSMK (1).svg";
 import "./Header.css";
-import list from "../../assets/list.jpeg"
+// import list from "../../assets/list.jpeg"
 function Header({ scrollToComponent }) {
   const [isSticky, setIsSticky] = useState(false);
 
